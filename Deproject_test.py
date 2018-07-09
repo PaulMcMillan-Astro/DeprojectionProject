881e9002966d6e096c8c539c9b02a002747a874c
Jooehn <Jooehn@users.noreply.github.com>
2018-07-09 17:13:28 +0200

    Added the finished functions to python scripts

---
+++ Deproject_test.py
@@ -0,0 +1,306 @@
+"""Test that for a given model sample checks if the distribution of L is smooth for a number of guesses of the dispersion."""
+
+def test_L(plane, alpha, v0, disp0, pvals, rhatvals, vmin, dv, n):
+    
+    dvx, dvy, dvz = dv
+    nx, ny, nz = n
+    disp0x, disp0y, disp0z = disp0
+    N = len(pvals)
+    
+    sigma2 = calc_sigma2(pvals,rhatvals) 
+    
+    Kvals = np.zeros((N,nx,ny,nz))
+    
+    for i in range(N):
+        K = calc_K(pvals[i],rhatvals[i],vmin,dv,n)
+        Kvals[i] += K 
+    
+    """We generate a set of sigma values for each dimension that will serve as basis for different gueses of phi"""
+    
+    step = 5
+    
+    dispx_guess = np.arange(0.6*disp0x,1.4*disp0x)
+    dispy_guess = np.arange(0.6*disp0y,1.4*disp0y)
+    dispz_guess = np.arange(0.6*disp0z,1.4*disp0z)
+    
+    if plane == 'xy':
+        trueind = np.where(disp0z == dispz_guess)
+        disp10, disp20 = disp0x, disp0y
+        disp1, disp2 = dispx_guess, dispy_guess
+        disp3 = dispz_guess[trueind]
+        n1, n2 = nx, ny
+        dx, dy = dvx, dvy
+        x0, y0 = min(dispx_guess), min(dispy_guess)
+        x1, y1 = max(dispx_guess), max(dispy_guess)
+        xlab, ylab = '$\sigma_x$', '$\sigma_y$'
+    elif plane == 'yz':
+        trueind = np.where(disp0x == dispz_guess)
+        disp10, disp20 = disp0y, disp0z
+        disp1, disp2 = dispy_guess, dispz_guess
+        disp3 = dispx_guess[trueind]
+        n1, n2 = ny, nz
+        dx, dy = dvy, dvz
+        x0, y0 = min(dispy_guess), min(dispz_guess)
+        x1, y1 = max(dispy_guess), max(dispz_guess)
+        xlab, ylab = '$\sigma_y$', '$\sigma_z$'
+    elif plane == 'xz':
+        trueind = np.where(disp0y == dispz_guess)
+        disp10, disp20 = disp0x, disp0z
+        disp1, disp2 = dispx_guess, dispz_guess
+        disp3 = dispy_guess[trueind]
+        n1, n2 = nx, nz
+        dx, dy = dvx, dvz
+        x0, y0 = min(dispx_guess), min(dispz_guess)
+        x1, y1 = max(dispx_guess), max(dispz_guess)
+        xlab, ylab = '$\sigma_x$', '$\sigma_z$'
+    
+    L_vals = np.zeros((len(disp1),len(disp2)))
+    
+    for i in range(len(disp1)):
+        
+        for j in range(len(disp2)):
+            
+            disp_guess = np.array([disp1[i],disp2[j],disp3])
+    
+            phi0 = phi_guess(v0,disp_guess,vmin,dv,n)
+
+            phi0r = np.ravel(phi0)
+
+            L_vals[i][j] += -get_L(phi0r, Kvals, N, alpha, dv, n, sigma2)
+            
+    fig, ax = plt.subplots(figsize=(8,6))
+    
+    dispg1, dispg2 = np.meshgrid(disp1,disp2)
+    
+    ax.set_title('Contour plot of $\mathcal{L}$ values')
+    ax.set_xlim(x0,x1)
+    ax.set_ylim(y0,y1)
+    ax.set_xlabel(xlab+' [km s$^{-1}$]',size='large')
+    ax.set_ylabel(ylab+' [km s$^{-1}$]',size='large')
+    
+    #contf = ax.contourf(dispg1,dispg2,L_vals.T,cmap = plt.cm.get_cmap('jet'),vmin=-40,vmax=10)
+    
+    extent = [disp1[0], disp1[-1], disp2[0], disp2[-1]]
+    im = ax.imshow(L_vals.T,origin='lower',interpolation='bilinear',cmap = plt.cm.get_cmap('jet'),extent=extent)
+    
+    cb = plt.colorbar(im,orientation='vertical', extend='max')
+    cb.set_label('Value of $\mathcal{L}$',size='large')
+    
+    ax.scatter(disp10,disp20,c='black')
+    
+    plt.show()
+    
+    return
+
+def sanity_check(pvals,rhatvals,phi,vmin,dv,n):
+    
+    """Test function that for a given pseudosample computes the Gaussian distribution and the corresponding distribution
+    found through maximization using the conjugate gradient algorithm."""
+    
+    dvx, dvy, dvz = dv
+    nx, ny, nz = n
+    vxmin, vymin, vzmin = vmin
+    vxmax, vymax, vzmax = vxmin+nx*dvx,vymin+ny*dvy,vzmin+nz*dvz
+    
+    vx_bins = np.arange(vxmin, vxmax+dvx, dvx)
+    vy_bins = np.arange(vymin, vymax+dvy, dvy)
+    vz_bins = np.arange(vzmin, vzmax+dvz, dvz)
+    
+    vxc = (vx_bins[1:]+vx_bins[:-1])/2
+    vyc = (vy_bins[1:]+vy_bins[:-1])/2
+    vzc = (vz_bins[1:]+vz_bins[:-1])/2
+
+    sigma2, vmean0 = calc_sigma2(pvals,rhatvals,True)
+    
+    sigma = np.sqrt(sigma2)
+    
+    phi0 = phi_guess(vmean0,sigma,vmin,dv,n)
+    fv0 = np.exp(phi0)
+    
+    fv = np.exp(phi)
+    
+    """Here we make sure to multipy the probability distribution f(v) with the centre values of the velocity bins in
+    each dimension. """
+    
+    vxmean = np.sum(vxc[None,:,None]*fv)/np.sum(fv)
+    vymean = np.sum(vyc[:,None,None]*fv)/np.sum(fv) 
+    vzmean = np.sum(vzc[None,None,:]*fv)/np.sum(fv)
+    vmean = np.array([vxmean,vymean,vzmean])
+    
+    dispx = np.sum((vxc[None,:,None]**2*fv))/np.sum(fv)-vxmean**2
+    dispy = np.sum((vyc[:,None,None]**2*fv))/np.sum(fv)-vymean**2
+    dispz = np.sum((vzc[None,None,:]**2*fv))/np.sum(fv)-vzmean**2
+    disp = np.sqrt(np.array([dispx,dispy,dispz]))
+    
+    #vxmean = np.sum(vxcg*fv)/np.sum(fv)
+    #vymean = np.sum(vycg*fv)/np.sum(fv)
+    #vzmean = np.sum(vzcg*fv)/np.sum(fv)
+    #vmean = np.array([vxmean,vymean,vzmean])
+    
+    #dispx = np.sum((vxcg**2*fv))/np.sum(fv)-vxmean**2
+    #dispy = np.sum((vycg**2*fv))/np.sum(fv)-vymean**2
+    #dispz = np.sum((vzcg**2*fv))/np.sum(fv)-vzmean**2
+    #disp = np.sqrt(np.array([dispx,dispy,dispz]))
+    
+    return vmean0, sigma, vmean, disp
+
+def L_collector(phi_all,vmin,dv,n,alpha=1e-5):
+    
+    dvx, dvy, dvz = dv
+    nx, ny, nz = n
+    vxmin, vymin, vzmin = vmin
+    N = len(pvals)
+    
+    sigma2 = calc_sigma2(pvals,rhatvals) 
+    
+    Kvals = np.zeros((N,nx,ny,nz))
+    
+    L_all = np.zeros(len(phi_all))
+    
+    for i in range(N):
+        K = calc_K(pvals[i],rhatvals[i],vmin,dv,n)
+        Kvals[i] += K 
+    
+    for i in range(len(phi_all)):
+        phi = np.ravel(phi_all[i])
+        L_all[i] += get_L(phi, Kvals, N, alpha, dv, n, sigma2)
+        
+    return -L_all
+
+def sanity_check(pvals,rhatvals,phi,vmin,dv,n):
+    
+    """Test function that for a given pseudosample computes the Gaussian distribution and the corresponding distribution
+    found through maximization using the conjugate gradient algorithm."""
+    
+    dvx, dvy, dvz = dv
+    nx, ny, nz = n
+    vxmin, vymin, vzmin = vmin
+    vxmax, vymax, vzmax = vxmin+nx*dvx,vymin+ny*dvy,vzmin+nz*dvz
+    
+    vx_bins = np.arange(vxmin, vxmax+dvx, dvx)
+    vy_bins = np.arange(vymin, vymax+dvy, dvy)
+    vz_bins = np.arange(vzmin, vzmax+dvz, dvz)
+    
+    vxc = (vx_bins[1:]+vx_bins[:-1])/2
+    vyc = (vy_bins[1:]+vy_bins[:-1])/2
+    vzc = (vz_bins[1:]+vz_bins[:-1])/2
+
+    sigma2, vmean0 = calc_sigma2(pvals,rhatvals,True)
+    
+    sigma = np.sqrt(sigma2)
+    
+    phi0 = phi_guess(vmean0,sigma,vmin,dv,n)
+    fv0 = np.exp(phi0)
+    
+    fv = np.exp(phi)
+    
+    """Here we make sure to multipy the probability distribution f(v) with the centre values of the velocity bins in
+    each dimension. """
+    
+    vxmean = np.sum(vxc[None,:,None]*fv)/np.sum(fv)
+    vymean = np.sum(vyc[:,None,None]*fv)/np.sum(fv) 
+    vzmean = np.sum(vzc[None,None,:]*fv)/np.sum(fv)
+    vmean = np.array([vxmean,vymean,vzmean])
+    
+    dispx = np.sum((vxc[None,:,None]**2*fv))/np.sum(fv)-vxmean**2
+    dispy = np.sum((vyc[:,None,None]**2*fv))/np.sum(fv)-vymean**2
+    dispz = np.sum((vzc[None,None,:]**2*fv))/np.sum(fv)-vzmean**2
+    disp = np.sqrt(np.array([dispx,dispy,dispz]))
+    
+    return vmean0, sigma, vmean, disp
+
+"""Test that for a given model sample checks if the distribution of L is smooth for a number of guesses of the dispersion."""
+
+def test_L(plane, alpha, v0, disp0, pvals, rhatvals, vmin, dv, n):
+    
+    dvx, dvy, dvz = dv
+    nx, ny, nz = n
+    disp0x, disp0y, disp0z = disp0
+    N = len(pvals)
+    
+    sigma2 = calc_sigma2(pvals,rhatvals) 
+    
+    Kvals = np.zeros((N,nx,ny,nz))
+    
+    for i in range(N):
+        K = calc_K(pvals[i],rhatvals[i],vmin,dv,n)
+        Kvals[i] += K 
+    
+    """We generate a set of sigma values for each dimension that will serve as basis for different gueses of phi"""
+    
+    step = 5
+    
+    dispx_guess = np.arange(0.6*disp0x,1.4*disp0x)
+    dispy_guess = np.arange(0.6*disp0y,1.4*disp0y)
+    dispz_guess = np.arange(0.6*disp0z,1.4*disp0z)
+        
+    #disp_xx, disp_yy, disp_zz = np.meshgrid(dispx_guess,dispy_guess,dispz_guess)
+    
+    if plane == 'xy':
+        trueind = np.where(disp0z == dispz_guess)
+        disp10, disp20 = disp0x, disp0y
+        disp1, disp2 = dispx_guess, dispy_guess
+        disp3 = dispz_guess[trueind]
+        n1, n2 = nx, ny
+        dx, dy = dvx, dvy
+        x0, y0 = min(dispx_guess), min(dispy_guess)
+        x1, y1 = max(dispx_guess), max(dispy_guess)
+        xlab, ylab = '$\sigma_x$', '$\sigma_y$'
+    elif plane == 'yz':
+        trueind = np.where(disp0x == dispz_guess)
+        disp10, disp20 = disp0y, disp0z
+        disp1, disp2 = dispy_guess, dispz_guess
+        disp3 = dispx_guess[trueind]
+        n1, n2 = ny, nz
+        dx, dy = dvy, dvz
+        x0, y0 = min(dispy_guess), min(dispz_guess)
+        x1, y1 = max(dispy_guess), max(dispz_guess)
+        xlab, ylab = '$\sigma_y$', '$\sigma_z$'
+    elif plane == 'xz':
+        trueind = np.where(disp0y == dispz_guess)
+        disp10, disp20 = disp0x, disp0z
+        disp1, disp2 = dispx_guess, dispz_guess
+        disp3 = dispy_guess[trueind]
+        n1, n2 = nx, nz
+        dx, dy = dvx, dvz
+        x0, y0 = min(dispx_guess), min(dispz_guess)
+        x1, y1 = max(dispx_guess), max(dispz_guess)
+        xlab, ylab = '$\sigma_x$', '$\sigma_z$'
+    
+    L_vals = np.zeros((len(disp1),len(disp2)))
+    
+    for i in range(len(disp1)):
+        
+        for j in range(len(disp2)):
+            
+            disp_guess = np.array([disp1[i],disp2[j],disp3])
+    
+            phi0 = phi_guess(v0,disp_guess,vmin,dv,n)
+
+            phi0r = np.ravel(phi0)
+
+            L_vals[i][j] += -get_L(phi0r, Kvals, N, alpha, dv, n, sigma2)
+            
+    fig, ax = plt.subplots(figsize=(8,6))
+    
+    dispg1, dispg2 = np.meshgrid(disp1,disp2)
+    
+    ax.set_title('Contour plot of $\mathcal{L}$ values')
+    ax.set_xlim(x0,x1)
+    ax.set_ylim(y0,y1)
+    ax.set_xlabel(xlab+' [km s$^{-1}$]',size='large')
+    ax.set_ylabel(ylab+' [km s$^{-1}$]',size='large')
+    
+    #contf = ax.contourf(dispg1,dispg2,L_vals.T,cmap = plt.cm.get_cmap('jet'),vmin=-40,vmax=10)
+    
+    extent = [disp1[0], disp1[-1], disp2[0], disp2[-1]]
+    im = ax.imshow(L_vals.T,origin='lower',interpolation='bilinear',cmap = plt.cm.get_cmap('jet'),extent=extent)
+    
+    cb = plt.colorbar(im,orientation='vertical', extend='max')
+    cb.set_label('Value of $\mathcal{L}$',size='large')
+    
+    ax.scatter(disp10,disp20,c='black')
+    
+    plt.show()
+    
+    return
---
+++ Deproject_v0.py
@@ -0,0 +1,214 @@
+"""Contains finalised functions from the Deprojection notebook"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import astropy.units as u
+import astropy.coordinates as coord
+import scipy.stats as st
+from astropy.table import Table
+from astropy.io import ascii
+from scipy.optimize import minimize
+from scipy.optimize import fmin_cg
+
+def model_sample(N,v0,disp0): 
+
+    """Generates a simple model solar neighbourhood star sample in a Galactic frame of reference assuming a 
+    Gaussian velocity distribution. The stars have distances from the Sun that are in the range [10,100] pc.
+    
+    Takes the following arugments:
+    
+    N: Number of stars in the sample
+    v0: 3d array specifying the mean velocities of the Gaussian distributions in x, y, z
+    disp: 3d array specifying the velocity dispersions of the distributions in x, y, z"""
+    
+    xmax, ymax, zmax = np.array([100,100,100]) / np.sqrt(3)
+    xmin, ymin, zmin = -xmax,-ymax,-zmax
+    
+    psx = (np.random.rand(N)*(xmax-xmin)+xmin)*u.pc
+    psy = (np.random.rand(N)*(ymax-ymin)+ymin)*u.pc
+    psz = (np.random.rand(N)*(zmax-zmin)+zmin)*u.pc
+    
+    scale = np.random.randn(N,3)
+    psvels = v0 + scale*disp0
+    
+    psvx, psvy, psvz = psvels.T
+    
+    psample = coord.Galactic(u=psx,v=psy,w=psz,U=psvx*(u.km/u.s),
+                             V=psvy*(u.km/u.s),W=psvz*(u.km/u.s),representation_type=coord.CartesianRepresentation,differential_type=coord.CartesianDifferential)
+    
+    psample.set_representation_cls(coord.SphericalRepresentation)
+    
+    return psample
+
+def calc_K(pk,rhat,vmin,dv,n):
+    '''Calculate the values of K simultaneously for all bins for a given star with p, rhat'''
+    
+    vxmin, vymin, vzmin = vmin
+    dvx, dvy, dvz = dv
+    nx, ny, nz = n
+    pkx, pky, pkz = pk
+    rhatx, rhaty, rhatz = rhat
+    
+    K = np.zeros((nx,ny,nz))
+    
+    vxmax, vymax, vzmax = vxmin+nx*dvx,vymin+ny*dvy,vzmin+nz*dvz
+    
+    """We now solve the line equation v = pk + vr*rhat, where v are the intersections of the tangential velocity line 
+    and the boundaries of each bin"""
+    
+    vx_bins = np.arange(vxmin, vxmax+dvx, dvx)
+    vy_bins = np.arange(vymin, vymax+dvy, dvy)
+    vz_bins = np.arange(vzmin, vzmax+dvz, dvz)
+    
+    vrx = (vx_bins-pkx)/rhatx
+    vry = (vy_bins-pky)/rhaty
+    vrz = (vz_bins-pkz)/rhatz
+    
+    """After solving the line equation for each dim we remove the vr values which solve the equation
+    for bins outside of our specified box."""
+    
+    vrmax = min(max(vrx),max(vry),max(vrz))
+    vrmin = max(min(vrx),min(vry),min(vrz))
+    
+    vrx = vrx[(vrx<=vrmax) & (vrx>=vrmin)]
+    vry = vry[(vry<=vrmax) & (vry>=vrmin)]
+    vrz = vrz[(vrz<=vrmax) & (vrz>=vrmin)]
+    vr = np.concatenate((vrx,vry,vrz))
+    vr.sort() #We obtain an ordered list with all vr values for intersections between entry and exit points
+    
+    if len(vr)==0:
+        return K
+    
+    vr_prime =(vr[:-1] + vr[1:]) / 2
+
+    pk = np.stack([pk]*len(vr_prime)) #The stacking allows us to solve line equations simultaneously
+    rhat = np.stack([rhat]*len(vr_prime))
+    vmin = np.stack([vmin]*len(vr_prime))
+    vr_primestack = np.stack([vr_prime]*3,axis=1)
+
+    """We now solve the line equation again for values in the middle of each bin with a line segment in it.
+    This gives us the coordinates for each relevant bin, given in line_bins.
+    Finally we compute the length of each segment and add said value to the relevant box in our K-space."""
+    v_prime = pk + vr_primestack*rhat
+    line_bins = np.floor((v_prime-vmin)/ dv)
+      
+    line_bins = line_bins.astype(int)
+    
+    line_len = vr[1:]-vr[:-1] #Gets the length of each line
+    non_zero = np.nonzero(line_len)
+    line_len = line_len[non_zero] #Removes duplicate intersections
+    line_bins = line_bins[non_zero]
+    
+    K[line_bins[:,0],line_bins[:,1],line_bins[:,2]] = line_len/(dvx*dvy*dvz)
+    
+    return K
+
+def calc_sigma2(pvals,rhat,give_vmean=False):
+    
+    """Function that applies equation 12 of DB98 for a set of stars from their tangential velocities and unit vectors.
+    Returns the velocity dispersion tensor."""
+    
+    pmean = np.mean(pvals, axis=0)
+    
+    rhat_outer = rhat[:,:,None]*rhat[:,None,:] #Fast way of getting the outer product for each rhat with itself.
+
+    iden = np.identity(3)
+    
+    A = np.stack([iden]*len(rhat_outer))-rhat_outer #Eq. 4 in DB98. Yields an array of dim (N_star,3,3)
+    
+    A_mean = np.mean(A,axis=0)
+    A_mean_inv = np.linalg.inv(A_mean)
+    v_mean = np.dot(A_mean_inv, pmean)
+    
+    pp = pvals - np.dot(A,v_mean)
+    
+    pp2mean = np.mean(pp*pp,axis=0)
+    
+    B = np.array([[9,-1,-1],[-1,9,-1],[-1,-1,9]])
+    
+    sigma2 = (3/14)*np.dot(B,pp2mean)
+    
+    if give_vmean == True:
+    
+        return sigma2, v_mean
+    
+    else:
+    
+        return sigma2
+    
+def sec_der(phi,sigma2,dv):
+    
+    """Estimates the second deriative for ln(f(v_l)) given a sample of stars (eq. 30 of D98).
+    Takes contributions at the phi values of adjacent bins for each bin l.
+    
+    We create a new, larger box with dimensions n+2 centred on our phi-space box.
+    This allows us to disregard any issues at the bins at the boundaries of our phi-space."""
+    
+    nx, ny, nz = phi.shape
+    dv2 = dv**2
+    
+    nxx, nyy, nzz = nx+2, ny+2, nz+2 
+
+    phip = np.zeros((nxx,nyy,nzz)) #new larger box
+
+    phip[1:-1,1:-1,1:-1] = phi #puts the phi-box in the centre of our larger box
+    
+    kappa = sigma2/dv2
+    
+    kappa_sum = -2*sum(sigma2/dv2)
+    
+    """Here we compute the contributions from all the adjacent bins simultaneously.
+    In every dimension we sum the phi values of box l-1 and l+1 and multiply with the relevant factor"""
+    
+    phi_fac = np.array([phip[0:nxx-2,1:-1,1:-1]+phip[2:nxx,1:-1,1:-1],
+                           phip[1:-1,0:nyy-2,1:-1]+phip[1:-1,2:nyy,1:-1],
+                           phip[1:-1,1:-1,0:nzz-2]+phip[1:-1,1:-1,2:nzz]])
+
+    phi_arrx = (sigma2[0]/dv2[0])*phi_fac[0]
+    phi_arry = (sigma2[1]/dv2[1])*phi_fac[1]
+    phi_arrz = (sigma2[2]/dv2[2])*phi_fac[2]
+    
+    """We sum all contributions from adjacent boxes and finally add the terms for each box l. 
+    Yields a box with the same dimensions as phi, containing the second derivative values for each bin."""
+    
+    phi_arr = phi_arrx+phi_arry+phi_arrz+kappa_sum*phi 
+
+    return phi_arr
+
+def phi_guess(v0,disp0,vmin,dv,n):
+    
+    """Provides an initial guess of the phi values in each bin given an assumed distribution f(v). 
+    For now only allows for a Gaussian type guess given arrays with mean velocities and dispersions for each dimension."""
+    
+    vxmin, vymin, vzmin = vmin
+    dvx, dvy, dvz = dv
+    nx, ny, nz = n
+    v0x, v0y, v0z = v0
+    dispx, dispy, dispz = disp0
+    
+    vxmax, vymax, vzmax = vxmin+nx*dvx,vymin+ny*dvy,vzmin+nz*dvz
+    
+    vx_bins = np.arange(vxmin, vxmax+dvx, dvx)
+    vy_bins = np.arange(vymin, vymax+dvy, dvy)
+    vz_bins = np.arange(vzmin, vzmax+dvz, dvz)
+    
+    vxc = (vx_bins[1:]+vx_bins[:-1])/2
+    vyc = (vy_bins[1:]+vy_bins[:-1])/2
+    vzc = (vz_bins[1:]+vz_bins[:-1])/2
+    
+    """Given the velocities of each bin we compute the 3D Gaussian value."""
+    
+    gx = st.norm(v0x,dispx) #Gets the normal distribution for dim x given the mean velocities v0x and dispersion dispx
+    gy = st.norm(v0y,dispy)
+    gz = st.norm(v0z,dispz)
+    
+    gxp = gx.logpdf(vxc) #Computes the log of the probability of our bin values being in the Gaussian
+    gyp = gy.logpdf(vyc)
+    gzp = gz.logpdf(vzc)    
+    
+    phi = np.meshgrid(gxp,gyp,gzp) #The meshgrid function couples each phi value for our 3D scenario to the relevant box 
+    
+    phi_sum = np.sum(phi,axis=0) #We sum the contributions to phi from x,y,z in each box
+    
+    return phi_sum
